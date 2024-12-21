24f8dc062638726efdd787041d96fd420d720052
staimov <staimov@mail.ru>
2024-12-21 21:58:43 +0500

    refactor: JsonSchemaValidator clsss added

--- src/main/java/sbp/school/kafka/utils/TransactionSerializer.java
+++ src/main/java/sbp/school/kafka/utils/TransactionSerializer.java
@@ -1,38 +1,32 @@
 package sbp.school.kafka.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.networknt.schema.JsonSchema;
-import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.SpecVersion;
-import com.networknt.schema.ValidationMessage;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sbp.school.kafka.dto.TransactionDto;
 
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
 
 /**
  * Класс сериализации транзакции
  */
 public class TransactionSerializer implements Serializer<TransactionDto> {
     private static final Logger logger = LoggerFactory.getLogger(TransactionSerializer.class);
+    public static final String SCHEMA_FILENAME = "/transaction-schema.json";
 
     private final ObjectMapper objectMapper;
-    private final JsonSchema schema;
+    private final JsonSchemaValidator validator;
 
     public TransactionSerializer() {
         this.objectMapper = new ObjectMapper();
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.registerModule(new JavaTimeModule());
-        this.schema = initializeSchema();
+
+        this.validator = new JsonSchemaValidator(SCHEMA_FILENAME, objectMapper);
     }
 
     /**
@@ -46,32 +40,14 @@ public class TransactionSerializer implements Serializer<TransactionDto> {
     public byte[] serialize(String topic, TransactionDto transaction) {
         if (transaction != null) {
             try {
-                String value = objectMapper.writeValueAsString(transaction);
-                validate(value);
-                return value.getBytes(StandardCharsets.UTF_8);
+                String jsonString = objectMapper.writeValueAsString(transaction);
+                validator.validate(jsonString);
+                return jsonString.getBytes(StandardCharsets.UTF_8);
             } catch (Exception e) {
                 logger.error("Ошибка сериализации: {}", e.getMessage());
                 throw new SerializationException(e);
             }
         }
         return null;
-    }
-
-    private void validate(String value) throws JsonProcessingException {
-        JsonNode jsonNode = objectMapper.readTree(value);
-        Set<ValidationMessage> validationResult = schema.validate(jsonNode);
-        if (!validationResult.isEmpty()) {
-            throw new RuntimeException(validationResult.toString());
-        }
-    }
-
-    private JsonSchema initializeSchema() {
-        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
-        try (InputStream schemaStream = getClass().getResourceAsStream("/transaction-schema.json")) {
-            return factory.getSchema(schemaStream);
-        } catch (Exception e) {
-            logger.error("Ошибка загрузки json-схемы: {}", e.getMessage());
-            throw new RuntimeException(e);
-        }
     }
 }
