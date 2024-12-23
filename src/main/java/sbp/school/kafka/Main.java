04b5a00b26489d10d9638c3812eeb4fb22730efe
staimov <staimov@mail.ru>
2024-12-23 15:00:50 +0500

    kafka_2: javadocs added to sbp.school.kafka.Main.main

--- src/main/java/sbp/school/kafka/Main.java
+++ src/main/java/sbp/school/kafka/Main.java
@@ -4,11 +4,17 @@ import sbp.school.kafka.config.KafkaConfig;
 import sbp.school.kafka.consumer.TransactionConsumer;
 
 public class Main {
+    /**
+     * Точка входа в приложение. Инициализирует и запускает потребителя Kafka сообщений.
+     *
+     * <p>Метод регистрирует shutdown hook для корректного завершения работы потребителя
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         KafkaConfig kafkaConfig = new KafkaConfig();
         try (TransactionConsumer consumer = new TransactionConsumer(kafkaConfig.getProperties())) {
 
-            // Добавляем поток, который запустится при завершении работы
             Runtime.getRuntime().addShutdownHook(new Thread(consumer::close));
 
             consumer.consume();
