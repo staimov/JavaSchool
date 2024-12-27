01800e1a2b847b1a96e3d8bfc02f1b57efea300b
staimov <staimov@mail.ru>
2024-12-27 18:45:09 +0500

    kafka_3: minor comment fix

--- src/main/java/sbp/school/kafka/Main.java
+++ src/main/java/sbp/school/kafka/Main.java
@@ -2,7 +2,7 @@ package sbp.school.kafka;
 
 public class Main {
     public static void main(String[] args) {
-        // Чтобы проверит работу запусти тест
+        // Чтобы проверить работу запусти тест
         // sbp.school.kafka.producer.TransactionProducerTest.testSendMultipleTransactions_And_WaitForAcks_And_RetrySend
     }
 }
